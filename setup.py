fb2152bf271b1f53f126f085351ddcd70550d3b7
nicoclavier <nicoclavier@me.com>
2016-06-09 11:53:10 +0200

    [fix] requires Requests 2.10.0

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setup(name='pyews',
       ],
       py_modules=['pyews'],
       tests_require=['nose>=1.0', 'coverage'],
-      install_requires=['requests==2.9.1', 'tornado==4.3'],
+      install_requires=['requests==2.10.0', 'tornado==4.3'],
       )
